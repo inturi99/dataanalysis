d48028b195ce8e533a5e65fd7361f2c09a902d38
Krishnarao Inturi <krishnarao1>
2016-03-13 12:03:38 +0530

    create parameterstimates,statepopulation table and seed data

---
+++ resources/migrations/20160313102145-create-parameterestimates.up.sql
@@ -0,0 +1,53 @@
+-- Name: parameterestimates;  Type: TABLE; Schema: public; Owner: postgres; Tablespace:
+CREATE TABLE IF NOT EXISTS  parameterestimates
+(
+id serial NOT NULL,
+type text,
+rate text,
+state  text,
+constant numeric,
+population numeric,
+gender numeric,
+CONSTRAINT "parameterestimates_pkey" PRIMARY KEY (id)
+);
+
+-- Data for Name: parameterestimates; Type: TABLE DATA; Schema: public; Owner: postgres
+
+INSERT INTO "parameterestimates"(id, type, rate, state, constant, population, gender)VALUES (1,	'UPS',	'LFPR', 'Andhra Pradesh', 630.7928,	0.01503, -324.497);
+INSERT INTO "parameterestimates"(id, type, rate, state, constant, population, gender)VALUES (2,	'UPS',	'LFPR',	'Arunachal Pradesh', 578.2706, 0.80114, -236.777);
+INSERT INTO "parameterestimates"(id, type, rate, state, constant, population, gender)VALUES (3, 'UPS',	'LFPR',  'Assam',	482.1277,	0.07846,	-558.412);
+INSERT INTO "parameterestimates"(id, type, rate, state, constant, population, gender)VALUES (4,	'UPS',	'LFPR',	'Bihar', 429.8194, 0.0265, -564.174);
+INSERT INTO "parameterestimates"(id, type, rate, state, constant, population, gender)VALUES (5,	'UPS',	'LFPR',	'Chhattisgarh',	619.6701,	0.06047,	-344.333);
+INSERT INTO "parameterestimates"(id, type, rate, state, constant, population, gender)VALUES (6,	'UPS',	'LFPR',	'Delhi', 399.9014,	0.09041, -432.2);
+INSERT INTO "parameterestimates"(id, type, rate, state, constant, population, gender)VALUES (7,	'UPS',	'LFPR',	'Goa', 497.8229, 0.89808,	-395.866);
+INSERT INTO "parameterestimates"(id, type, rate, state, constant, population, gender)VALUES (8,	'UPS',	'LFPR',	'Gujarat', 492.9431,	0.03558	,-517.42);
+INSERT INTO "parameterestimates"(id, type, rate, state, constant, population, gender)VALUES (9,	'UPS',	'LFPR',	'Haryana',	392.7419,	0.08091,	-494.033);
+INSERT INTO "parameterestimates"(id, type, rate, state, constant, population, gender)VALUES (10, 'UPS',	'LFPR',	'Himachal Pradesh',	623.1075,	0.15089,	-261.291);
+INSERT INTO "parameterestimates"(id, type, rate, state, constant, population, gender)VALUES (11, 'UPS',	'LFPR',	'Jammu & Kashmir',	412.1829,	0.18185,	-51.047);
+INSERT INTO "parameterestimates"(id, type, rate, state, constant, population, gender)VALUES (12,	'UPS',	'LFPR',	'Jharkhand',	464.202,	0.07684, -554.008);
+INSERT INTO "parameterestimates"(id, type, rate, state, constant, population, gender)VALUES (13,	'UPS',	'LFPR',	'Karnataka',	548.6872,	0.02992,	-447.257);
+INSERT INTO "parameterestimates"(id, type, rate, state, constant, population, gender)VALUES (14,	'UPS',	'LFPR',	'Kerala',	492.9691,	0.05439,	-497.768);
+INSERT INTO "parameterestimates"(id, type, rate, state, constant, population, gender)VALUES (15,	'UPS',	'LFPR',	'Madhya Pradesh',	534.7028,	0.02852,	-464.612);
+INSERT INTO "parameterestimates"(id, type, rate, state, constant, population, gender)VALUES (16,	'UPS',	'LFPR',	'Maharashtra',	533.5615,	0.01306,	-363.07);
+INSERT INTO "parameterestimates"(id, type, rate, state, constant, population, gender)VALUES (17,	'UPS',	'LFPR',	'Manipur',	40.7836,	0.67034,	-393.127);
+INSERT INTO "parameterestimates"(id, type, rate, state, constant, population, gender)VALUES (18,	'UPS',	'LFPR',	'Meghalaya',	611.2133,	0.39783,	-252.141);
+INSERT INTO "parameterestimates"(id, type, rate, state, constant, population, gender)VALUES (19,	'UPS',	'LFPR',	'Mizoram',	623.4772,	0.89686,	-214.842);
+INSERT INTO "parameterestimates"(id, type, rate, state, constant, population, gender)VALUES (20,	'UPS',	'LFPR',	'Nagaland',	515.7602,	0.45503,	-241.552);
+INSERT INTO "parameterestimates"(id, type, rate, state, constant, population, gender)VALUES (21,	'UPS',	'LFPR',	'Orissa',	488.1495,	0.05816,	-579.251);
+INSERT INTO "parameterestimates"(id, type, rate, state, constant, population, gender)VALUES (22,	'UPS',	'LFPR',	'Punjab',	399.7249,	0.07561,	-523.6);
+INSERT INTO "parameterestimates"(id, type, rate, state, constant, population, gender)VALUES (23,	'UPS',	'LFPR',	'Rajasthan',	449.6058,	0.03153,	-481.366);
+INSERT INTO "parameterestimates"(id, type, rate, state, constant, population, gender)VALUES (24,	'UPS',	'LFPR',	'Sikkim',	588.5352,	2.11592,	-303.819);
+INSERT INTO "parameterestimates"(id, type, rate, state, constant, population, gender)VALUES (25,	'UPS',	'LFPR',	'Tamil Nadu',	573.3271,	0.02411,	-423.077);
+INSERT INTO "parameterestimates"(id, type, rate, state, constant, population, gender)VALUES (26,	'UPS',	'LFPR',	'Telangana',	0,	0,	0);
+INSERT INTO "parameterestimates"(id, type, rate, state, constant, population, gender)VALUES (27,	'UPS',	'LFPR',	'Tripura',	525.5863,	0.59235,	-515.843);
+INSERT INTO "parameterestimates"(id, type, rate, state, constant, population, gender)VALUES (28,	'UPS',	'LFPR',	'Uttarakhand',	450.071,	0.20712,	-492.459);
+INSERT INTO "parameterestimates"(id, type, rate, state, constant, population, gender)VALUES (29,	'UPS',	'LFPR',	'Uttar Pradesh',	350.5511,	0.01275,	-537.468);
+INSERT INTO "parameterestimates"(id, type, rate, state, constant, population, gender)VALUES (30,	'UPS',	'LFPR',	'West Bengal',	845.8581,	-0.00112,	-135.331);
+INSERT INTO "parameterestimates"(id, type, rate, state, constant, population, gender)VALUES (31,	'UPS',	'LFPR',	'A & N Islands',	495.8229,	2.96677,	-357.881);
+INSERT INTO "parameterestimates"(id, type, rate, state, constant, population, gender)VALUES (32,	'UPS',	'LFPR',	'Chandigarh',	408.01,	1.05,	-376.04);
+INSERT INTO "parameterestimates"(id, type, rate, state, constant, population, gender)VALUES (33,	'UPS',	'LFPR',	'Dadra & Nagar Haveli',	470.6203,	5.32622,	-474.328);
+INSERT INTO "parameterestimates"(id, type, rate, state, constant, population, gender)VALUES (34,	'UPS',	'LFPR',	'Daman & Diu',	390.5493,	5.15101,	-320.553);
+INSERT INTO "parameterestimates"(id, type, rate, state, constant, population, gender)VALUES (35,	'UPS',	'LFPR',	'Lakshadweep',	398.2617,	32.88728,	-584.274);
+INSERT INTO "parameterestimates"(id, type, rate, state, constant, population, gender)VALUES (36,	'UPS',	'LFPR',	'Puducherry',	513.1629,	1.14262,	-415.083);
+
+
---
+++ resources/migrations/20160313114241-create-statepopulation.up.sql
@@ -0,0 +1,59 @@
+-- Name: statepopulation; Type: TABLE; Schema: public; Owner: postgres; Tablespace:
+CREATE TABLE IF NOT EXISTS  statepopulation
+(
+type text,
+year int,
+state text,
+population bigint,
+gender int,
+lfpr numeric,
+wpr numeric
+
+);
+
+-- Data for Name: statepopulation; Type: TABLE DATA; Schema: public; Owner: postgres
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2012,	'Andhra Pradesh',	64204,	3,	641,	621);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2012,	'Andhra Pradesh',	32102,	1,	782,	761);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2012,	'Andhra Pradesh',	32015,	2,	494,	477);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2013,	'Andhra Pradesh',	64758,	3,	605,	590);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2013,	'Andhra Pradesh',	32367,	1,	776,	758);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2013,	'Andhra Pradesh',	32304,	2,	433,	422);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2014,	'Andhra Pradesh',	65301,	3,	645,	622);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2014,	'Andhra Pradesh',	32626,	1,	820,	795);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2014,	'Andhra Pradesh',	32588,	2,	474,	454);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2015,	'Andhra Pradesh',	65834,	3,	646.8,	0);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2016,	'Andhra Pradesh',	68126,	3,	681.2,	0);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2017,	'Andhra Pradesh',	68646,	3,	689.1,	0);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2018,	'Andhra Pradesh',	69152,	3,	696.7,	0);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2019,	'Andhra Pradesh',	69644,	3,	704.1,	0);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2020,	'Andhra Pradesh',	70122,	3,	711.2,	0);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2021,	'Andhra Pradesh',	71774,	3,	736.1,	0);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2022,	'Andhra Pradesh',	72215,	3,	742.7,	0);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2023,	'Andhra Pradesh',	72637,	3,	749.0,	0);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2024,	'Andhra Pradesh',	73037,	3,	755.0,	0);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2025,	'Andhra Pradesh',	73411,	3,	760.7,	0);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2026,	'Andhra Pradesh',	75070,	3,	785.6,	0);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2015,	'Andhra Pradesh',	32879,	1,	800.5,	0);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2016,	'Andhra Pradesh',	33926,	1,	816.2,	0);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2017,	'Andhra Pradesh',	34173,	1,	819.9,	0);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2018,	'Andhra Pradesh',	34414,	1,	823.5,	0);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2019,	'Andhra Pradesh',	34649,	1,	827.1,	0);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2020,	'Andhra Pradesh',	34877,	1,	830.5,	0);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2021,	'Andhra Pradesh',	35878,	1,	845.5,	0);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2022,	'Andhra Pradesh',	36087,	1,	848.7,	0);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2023,	'Andhra Pradesh',	36286,	1,	851.7,	0);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2024,	'Andhra Pradesh',	36473,	1,	854.5,	0);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2025,	'Andhra Pradesh',	36646,	1,	857.1,	0);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2026,	'Andhra Pradesh',	37366,	1,	867.9,	0);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2015,	'Andhra Pradesh',	32866,	2,	475.8,	0);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2016,	'Andhra Pradesh',	34022,	2,	493.1,	0);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2017,	'Andhra Pradesh',	34293,	2,	497.2,	0);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2018,	'Andhra Pradesh',	34557,	2,	501.2,	0);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2019,	'Andhra Pradesh',	34814,	2,	505.1,	0);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2020,	'Andhra Pradesh',	35063,	2,	508.8,	0);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2021,	'Andhra Pradesh',	35942,	2,	522.0,	0);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2022,	'Andhra Pradesh',	36174,	2,	525.5,	0);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2023,	'Andhra Pradesh',	36397,	2,	528.8,	0);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2024,	'Andhra Pradesh',	36610,	2,	532.0,	0);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2025,	'Andhra Pradesh',	36811,	2,	535.1,	0);
+INSERT INTO "statepopulation"(type, year, state, population, gender, lfpr, wpr) VALUES ('UPS',	2026,	'Andhra Pradesh',	37657,	2,	547.8,	0);
